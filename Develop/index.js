ad54556ecc5b9e78a331c0715c51df788399cfd9
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-09-07 09:29:58 -0700

    update inquirer version, inquirer prompts, license switch cases

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,52 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+   return inquirer .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your application?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description explaining the what, why, and how of your application.',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'How does the user install your application? Provide a step-by-step description.',
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Include guidelines for other developers who would like to contribute to your application.',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Provide examples on how to run the tests you built for your application.',
+        },
+        {
+            type: 'checkbox',
+            message: 'Which license will you use for your application?',
+            name: 'license',
+            choices: ['Apache License 2.0', 'MIT License', 'GNU GPLv3', 'I don’t want a license.'],
+        },
+      
+    ]);
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    promptUser()
+    .then((content) => fs.writeFileSync('README.md', generateREADME(content)))
+    .then(() => console.log('README successfully generated!'))
+    .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case 'Apache License 2.0':
+      // append badge to README [![Apache License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    break;
+    case 'MIT License':
+      // append badge to README [![MIT License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    break;
+    case 'GNU GPLv3':
+      // append badge to README [![GNU GPLv3 License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    break;
+    case 'I don’t want a license.':
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,10 +25,14 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+const generateREADME = (content) => {
+  const license = content.license
+  console.log(content);
+  console.log(license);
+  // `# ${content.title}`
+  // renderLicenseBadge(license);
+  // renderLicenseLink(license);
+  // renderLicenseSection(license);
 }
 
-module.exports = generateMarkdown;
+module.exports = generateREADME;
